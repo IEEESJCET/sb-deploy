6b0c083c133a2102c340d63d8cc8d58221a98269
jayasankar-shyam <jayasankar06shyam@gmail.com>
2021-12-22 08:05:55 +0530

    added summitium

---
+++ summitium/js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2021, 12, -3, 6)}); // year, month, date, hour
+        });
+});		
+
