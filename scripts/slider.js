85db63999b680497a7cd314475edae2901c9eb5c
Anish V Mathew <anishvmathew02@gmail.com>
2024-09-02 13:10:45 +0530

    event section update

---
+++ scripts/slider.js
@@ -0,0 +1,21 @@
+$('#posterCarousel').carousel({
+    interval: 5000
+  })
+
+  $('.carousel .carousel-item').each(function () {
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < minPerSlide; i++) {
+      next = next.next();
+      if (!next.length) {
+        next = $(this).siblings(':first');
+      }
+
+      next.children(':first-child').clone().appendTo($(this));
+    }
+  });
